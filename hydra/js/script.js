bbf61743673d4235aebbd2dfca3bc7b26748277b
tkachenko6591 <tkachenko6591@gmail.com>
2025-07-06 19:02:58 -0300

    add: pr-hydra

---
+++ hydra/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('click', documentActions)
+
+function documentActions(e) {
+	const targetElement = e.target
+	// if (targetElement.closest('.menu__arrow')) {
+	// 	targetElement.closest('.menu__item').classList.toggle('active')
+	// }
+	if (targetElement.closest('.icon-menu')) {
+		document.body.classList.toggle('menu-open')
+	}
+}
